2075ac69b1e641964d0fd8515fc63ab13a20100b
Marco Fiorito <marco@xmartlabs.com>
2020-12-30 20:04:32 -0300

    hello world navigation

---
+++ src/scenes/home/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {SafeAreaView, View, Text, Image} from 'react-native';
+import {styles} from './styles';
+import logo from '../../assets/images/logo.jpeg';
+
+export const Home = () => {
+  return (
+    <SafeAreaView>
+      <View style={styles.body}>
+        <View style={styles.sectionContainer}>
+          <Text style={styles.sectionTitle}>Butterfly</Text>
+          <Text style={styles.sectionDescription}>
+            <Text style={styles.highlight}>MetaLabs</Text> base project
+          </Text>
+        </View>
+        <View style={styles.imageContainer}>
+          <Image style={styles.logo} source={logo} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
---
+++ src/scenes/home/styles.ts
@@ -0,0 +1,33 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  body: {
+    backgroundColor: 'white',
+  },
+  sectionContainer: {
+    padding: 24,
+    borderBottomColor: 'gray',
+    borderBottomWidth: 1,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: 'black',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: 'dimgray',
+  },
+  imageContainer: {
+    paddingHorizontal: 24,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  logo: {
+    width: 150,
+    height: 150,
+  },
+});
